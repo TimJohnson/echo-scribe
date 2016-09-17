c076b369b59a7ca765bc5c706fbe66ad26013ee4
Tim Johnson <tjohn92@gmail.com>
2016-09-17 16:23:50 -0400

    added functions

--- server/echoscribe_service/public/js/echoscribe-ui.js
+++ server/echoscribe_service/public/js/echoscribe-ui.js
@@ -6,10 +6,11 @@ var userBetsy = $('#userBetsy');
 var meetingId = $('#meetingId');
 var meetingSelector = $('#meetingSelector');
 
-function showRecordingUI() {
+function showRecordingUI(roomName) {
 	$(uiWrap).addClass('is-recording');
 	$(recordingSplash).addClass('in');
 	
+	$(meetingId).html(roomName ? roomName + '<div>3&ndash;4pm</div>' : 'Red Ventures <div>3&ndash;4pm</div>');
 	setTimeout(function(){
 		$(mainRecording).show().addClass('in');
 		startTimer();
@@ -37,12 +38,43 @@ function startupScripts() {
 	$(stagingCenterWrap).addClass('in');
 }
 
+function startTranscription() {
+	var recognition = new webkitSpeechRecognition();
+	recognition.continuous = true;
+	recognition.interimResults = true;
+	recognition.lang = "en-US";
+  recognition.start();
+	
+	recognition.onresult = function (e) {
+        var textarea = document.getElementById('mySpeech');
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                textarea.innerHTML += e.results[i][0].transcript;
+            }
+        }
+    }
+}
+
+function stopTranscription() {
+	var recognition = new webkitSpeechRecognition();
+	recognition.continuous = true;
+	recognition.interimResults = true;
+	recognition.lang = "en-US";
+  recognition.stop();
+	
+	$(uiWrap).removeClass('is-recording');
+	$(recordingSplash).addClass('out');
+	
+	setTimeout(function(){
+		$(mainRecording).show().addClass('out');
+		startTimer();
+	}, 2500);
+}
 
 // Doc ready
 $( document ).ready(function() {
 
 	startupScripts();
-	betsyAdded();
 	meetingFactIsNotAReason();
 
 
